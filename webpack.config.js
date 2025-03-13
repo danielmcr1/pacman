b36e80962ad80467e29f04cbcb0f19eef99accba
Daniel Olufunmi <daniel.olufunmi@accenture.com>
2025-03-13 14:58:13 +0000

    modified webpack to use correct plugins

--- webpack.config.js
+++ webpack.config.js
@@ -3,43 +3,44 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.join(__dirname, '/dist'),
-    publicPath: "/dist/",
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'bundle.js',
+    clean: true, // Cleans 'dist' folder before each build
   },
   resolve: {
-    modules: ["node_modules"]
+    modules: ['node_modules'],
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules)/,
+        exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
+          loader: 'babel-loader',
+        },
       },
       {
-        test: /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
-        loader: 'file-loader'
+        test: /\.(ttf|eot|svg|woff2?)$/,
+        type: 'asset/resource',
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html' // Copies index.html to dist/
+      template: './src/index.html', // Copies index.html to dist/
     }),
     new CopyWebpackPlugin({
       patterns: [
         { from: './src/index.css', to: 'index.css' }, // Copies index.css to dist/
-        { from: './src/assets', to: 'assets' } // Copies assets folder to dist/
-      ]
-    })
-  ]
+        { from: './src/assets', to: 'assets' }, // Copies assets folder to dist/
+      ],
+    }),
+  ],
 };
